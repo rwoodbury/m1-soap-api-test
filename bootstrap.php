f1774183dc345c34675466af6807562f15810fb2
rwoodbury <rwoodbury@magento.com>
2016-06-16 16:16:35 -0700

    some initial setup

---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Convert all errors into ErrorExceptions
+ */
+set_error_handler(
+	function ($severity, $errstr, $errfile, $errline) {
+		throw new ErrorException($errstr, 1, $severity, $errfile, $errline);
+	},
+	E_USER_ERROR
+);
+
+/**
+ * Set handler for uncaught exceptions.
+ */
+set_exception_handler(
+	function ( Exception $e ) {
+		cerr( $e->getMessage() . PHP_EOL );
+		cerr( $e . PHP_EOL );
+		exit( $e->getCode() );
+	}
+);
+
+require_once __DIR__ . '/vendor/autoload.php';
