15e605c0d818b969e0635a83e3d6738629c81ab3
Reid Woodbury <rwoodbury@magento.com>
2016-06-17 15:33:35 -0700

    added comment

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,9 @@
 
 class ExampleTest extends PHPUnit\Framework\TestCase
 {
+	/**
+	 * This test causes a failure outputting the instructional string below.
+	 */
 	function testExample()
 	{
 		$this->expectOutputString('');
