087471d77f220c6bb696777c1323b6686ed981e1
Reid Woodbury <rwoodbury@magento.com>
2017-01-24 13:14:12 -0800

    playing with details

--- tests/example.php
+++ tests/example.php
@@ -1,10 +1,12 @@
 <?php
 
-//	Print a list of available API method calls.
+// Print a list of available API method calls.
 $res = $apiClient->getClient()->resources($apiClient->getSession());
-// print_r($res);
-foreach ( $res as $r ) {
-	foreach ( $r['methods'] as $m ) {
-		pout($m['path'] . PHP_EOL);
-	}
-}
+print_r($res);
+// foreach ( $res as $r ) {
+// 	foreach ( $r['methods'] as $m ) {
+// 		pout($m['path'] . PHP_EOL);
+// 	}
+// }
+
+// echo $apiClient->cataloginventoryStockItemUpdate('SUP151', ['qty'=>27, 'is_in_stock'=>1]);
